bdf20d287c337ffbbb755c04e0cedada7653f90d
William Luke <william@ordino.ai>
2020-10-06 15:27:54 +0300

    ?

--- pages/songs/[id].tsx
+++ pages/songs/[id].tsx
@@ -1,7 +1,6 @@
 import { Box, Button, Heading, Text } from '@chakra-ui/core';
 import NextLink from 'next/link';
 import { PrismaClient } from '../../db';
-import { InferGetStaticPropsType } from "next";
 
 const prisma = new PrismaClient();
 
@@ -33,7 +32,7 @@ export async function getStaticPaths() {
   };
 }
 
-export default ({ song }: InferGetStaticPropsType<typeof getStaticProps>) => (
+export default ({ song }) => (
   <Box mt={8}>
     <Heading fontWeight="800">{song.name}</Heading>
     <Text color="grey.700" mb={4}>
