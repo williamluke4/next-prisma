86c195c2e1d9ca7dd1b691877d9de235984fb48d
William Luke <william@ordino.ai>
2020-10-19 12:34:38 +0300

    woops

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -4,11 +4,3 @@ export default async function handle(req, res) {
   const {stdout} = await execa('ls', ['-a']);
   return res.json(stdout);
 }
-.
-..
-db
-.next
-node_modules
-now__bridge.js
-now__launcher.js
-package.json
