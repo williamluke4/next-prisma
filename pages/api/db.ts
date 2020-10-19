333f65e02981aab305438a01a676bf2b52139adc
William Luke <william@ordino.ai>
2020-10-19 12:56:07 +0300

    test

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -3,6 +3,6 @@ import path from 'path';
 
 export default async function handle(req, res) {
   const { stdout } = await execa('ls', ['-a'], { cwd: path.join(process.cwd(), 'db') });
-  const data = { cwd: process.cwd(), stdout: stdout }
+  const data = { cwd: process.cwd(), stdout: stdout, req: require.resolve('../../db') }
   return res.json(data);
 }
