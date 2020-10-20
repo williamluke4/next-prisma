3725cefa1f0027029b51578affdef8e7baca7b35
William Luke <william@ordino.ai>
2020-10-20 09:08:46 +0300

    b

--- pages/api/parent.ts
+++ pages/api/parent.ts
@@ -2,6 +2,6 @@ import execa from 'execa';
 import path from 'path';
 
 export default async function handle(req, res) {
-  const { stdout } = await execa('ls', ['-a'], { cwd: path.join(process.cwd(), '../') });
+  const { stdout } = await execa('ls', ['-a'], { cwd: path.join(process.cwd(), '../..') });
   return res.json(stdout);
 }
