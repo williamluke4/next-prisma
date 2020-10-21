125ca8f71e48630b8bb3724b1afae806eddd8d94
William Luke <william@ordino.ai>
2020-10-21 12:06:24 +0300

    -_-

--- pages/api/db.ts
+++
@@ -1,8 +0,0 @@
-import execa from 'execa';
-import path from 'path';
-
-export default async function handle(req, res) {
-  const { stdout } = await execa('ls', ['-a'], { cwd: path.join(process.cwd(), 'db') });
-  const data = { cwd: process.cwd(), stdout: stdout, req: require.resolve('../../db') }
-  return res.json(data);
-}
