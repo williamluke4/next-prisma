b44fe2f33b60e5b763010602d45fed0787e73ec3
William Luke <william@atto-byte.com>
2020-10-21 19:36:02 +0300

    Update tree.ts

--- pages/api/tree.ts
+++ pages/api/tree.ts
@@ -2,7 +2,7 @@
 import execa from 'execa';
 
 export default async function handle(req, res) {
-  const { stdout } = await execa('du', [process.cwd(), `--max-depth=4`]
+  const { stdout } = await execa('du', [process.cwd(), `--max-depth=4`])
   const data = { cwd: process.cwd(), stdout: stdout }
   return res.json(data);
 }
