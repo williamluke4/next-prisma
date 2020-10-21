09beba1cfad2343f990c2a1b8aaa883ae585c014
William Luke <william@atto-byte.com>
2020-10-21 19:34:32 +0300

    Create tree.ts

---
+++ pages/api/tree.ts
@@ -0,0 +1,8 @@
+  
+import execa from 'execa';
+
+export default async function handle(req, res) {
+  const { stdout } = await execa('du', [process.cwd(), `--max-depth=4`]
+  const data = { cwd: process.cwd(), stdout: stdout }
+  return res.json(data);
+}
