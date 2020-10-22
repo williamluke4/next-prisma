f4f4097fdca93fb52687d49e4f1436bd3882d7d6
William Luke <william@ordino.ai>
2020-10-22 10:05:37 +0300

    hardcode

--- pages/api/songs.ts
+++ pages/api/songs.ts
@@ -1,6 +1,7 @@
+import path from 'path';
 import { PrismaClient } from '../../db';
 const prisma = new PrismaClient();
-
+path.join('../../db/query-engine-rhel-openssl-1.0.x')
 export default async function handle(req, res) {
   const songs = await prisma.song.findMany({ include: { artist: true } });
   return res.json(songs);
