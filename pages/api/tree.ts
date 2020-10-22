fe5d4ab8a91644555e86ed0148cbbc2666f5f5f3
William Luke <william@ordino.ai>
2020-10-22 10:15:51 +0300

    up

--- pages/api/tree.ts
+++ pages/api/tree.ts
@@ -1,8 +1,9 @@
   
 import execa from 'execa';
+import path from 'path';
 
 export default async function handle(req, res) {
-  const { stdout } = await execa('du', [process.cwd(), `--max-depth=4`])
+  const { stdout } = await execa('du', [path.join(process.cwd(), '../'), `--max-depth=4`])
   const data = { cwd: process.cwd(), stdout: stdout }
   return res.json(data);
 }
---
+++ pages/tree.tsx
@@ -0,0 +1,18 @@
+import { Heading, List } from '@chakra-ui/core';
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default (props) => {
+  const { data: tree } = useSWR('/api/tree', fetcher);
+  return (
+    <>
+      <Heading mt={8} mb={4} fontWeight="800">
+        My Songs
+      </Heading>
+      <List>
+        {tree && tree.stdout}
+      </List>
+    </>
+  );
+};
